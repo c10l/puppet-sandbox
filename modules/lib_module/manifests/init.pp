18f637c049af85459f25c3209c50e289dcde50f4
Cassiano Leal <cassiano.leal@gamesys.co.uk>
2014-02-21 15:41:15 +0000

    Making hiera work

--- modules/lib_module/manifests/init.pp
+++ modules/lib_module/manifests/init.pp
@@ -4,4 +4,19 @@ class lib_module(
   notify { "arbitrary_parameter":
     message => "arbitrary_parameter is set to the ${arbitrary_parameter}"
   }
+
+  $hiera_param = hiera("lib_module::arbitrary_parameter")
+  notify { "hiera_lookup":
+    message => "hiera has lib_module::arbitrary_parameter set to ${hiera_param}"
+  }
+
+  if $arbitrary_parameter == $hiera_param {
+    notify { "match":
+      message => "arbitrary_parameter matches hiera_param!"
+    }
+  } else {
+    notify { "no_match":
+      message => "parameters DO NOT match..."
+    }
+  }
 }
